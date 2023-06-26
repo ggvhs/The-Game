709ef9882462b0196e63c545412aac1fcbe7ab20
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-06-26 15:55:26 -0700

    commit

--- app.js
+++ app.js
@@ -4,12 +4,55 @@ class wizard{
     constructor(name, hp , mp){
         this.name = name;
         this.hp = hp;
-        this.maxhp = 100;
+        this.maxHp = 100;
         this.mp = mp;
+        this.maxMp = 100;
     }
+
+    //A full heal for the wizard
+    heal(){
+        this.hp = this.maxHp;
+        console.log(`${this.name} now has full hp ${this.hp}`)
+    }
+
+    //A full mp up for the wizard
+    mpUp(){
+        this.mp = this.maxMp
+        console.log(`${this.name} now has full mp`)
+    }
+
+
 }
+
+
+
+const bobbyWizard = new wizard('Bobby',50,50)
+
+bobbyWizard.displayHealth()
 
 //Scene Class that changes the background and story options
-class scene{
+class Chapter{
+    constructor(){
+        this.currentChapter = 1
+    }
+
+    // startChapter(){
+    //     document.getElementById('title-screen').style.display = 'none';
+    //     document.getElementById('chapter-screen').style.display = 'block';
+
+    //     this.loadChapter(this.currentChapter);
+    // }
+
+    loadChapter(sceneNumber){
+        if (sceneNumber === 1){
+
+        }
+    }
+
 
 }
+
+
+
+
+let gameStory = new Chapter()
