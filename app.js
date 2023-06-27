4cb4deced122df793b9b27d2e130257a366f5d3c
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-06-26 20:51:18 -0700

    commit

--- app.js
+++ app.js
@@ -21,38 +21,85 @@ class wizard{
         console.log(`${this.name} now has full mp`)
     }
 
+    //Allows the wizard to be damaged
+    getHit(ammount){
+        this.hp -= ammount
+        console.log(`${this.name} lost ${ammount} health. Current HP ${this.hp}`)
+    }
+
+
+
+    //A reusable function that will be called anytime health can be effected
+    displayHealth(divId) {
+        let healthDiv = document.getElementById(divId);
+        healthDiv.innerText = `Health: ${this.hp}`;
+    }
+
 
 }
-
 
 
 const bobbyWizard = new wizard('Bobby',50,50)
 
-bobbyWizard.displayHealth()
-
-//Scene Class that changes the background and story options
-class Chapter{
-    constructor(){
-        this.currentChapter = 1
-    }
-
-    // startChapter(){
-    //     document.getElementById('title-screen').style.display = 'none';
-    //     document.getElementById('chapter-screen').style.display = 'block';
-
-    //     this.loadChapter(this.currentChapter);
-    // }
-
-    loadChapter(sceneNumber){
-        if (sceneNumber === 1){
-
-        }
-    }
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    bobbyWizard.displayHealth('health-div');
+  });
+
+
+
+function takeGoblinDamage(){
+    bobbyWizard.getHit(20);
 }
 
 
 
 
-let gameStory = new Chapter()
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Scene Class that changes the background and story options
+// class Chapter{
+//     constructor(){
+//         this.currentChapter = 1
+//     }
+
+//     startChapter(){
+//         document.getElementById('title-screen').style.display = 'none';
+//         document.getElementById('chapter-screen').style.display = 'block';
+
+//         this.loadChapter(this.currentChapter);
+//     }
+
+//     loadChapter(sceneNumber){
+//         if (sceneNumber === 1){
+//             document.getElementById('chapter-image').style.backgroundImage = 'url(./images/grassyLandscape.jpg)'
+//             this.currentChapter++;
+//             this.continueChapter(this.currentChapter)
+//         }
+//     }
+
+//     continueChapter(sceneNumber){
+//         if( sceneNumber === 2){
+
+//         }
+//     }
+
+
+
+// }
+
+
+// const gameStory = new Chapter()
