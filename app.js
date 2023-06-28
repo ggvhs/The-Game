5303444950ebaccac74f7f0420601284a3ccec8f
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-06-27 21:57:06 -0700

    functioning code that interacts with the dom using some re-usable methods

--- app.js
+++ app.js
@@ -27,4 +27,32 @@ class wizard{
         console.log(`${this.name} lost ${ammount} health. Current HP ${this.hp}`)
     }
 
+
+
+  
+
 }
+const bobbyWizard = new wizard('Bobby',50,50)
+
+const changeSceneFromTitleToGrass = function(){
+    let fromTitle = document.querySelector('#title-screen')
+    fromTitle.style.display = 'none';
+    let toGrass = document.querySelector('#grass');
+    toGrass.style.display = 'flex';
+}
+
+const toGrass = document.getElementById('toGrass')
+
+toGrass.addEventListener('click' , changeSceneFromTitleToGrass)
+
+
+const theRestButton = document.getElementById('reset');
+
+theRestButton.addEventListener('click', function() {
+   const grandparentDiv = theRestButton.parentNode.parentNode;
+   grandparentDiv.style.display = 'none';
+   const toTitle = document.querySelector('#title-screen')
+   toTitle.style.display = 'flex';
+   bobbyWizard.heal();
+})
+
