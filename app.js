a7b4a4f5e54c727cb6f18ebcce5baf7c4c39fb66
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-06-28 11:49:40 -0700

    user can now see wizards health and reset buttons function on all pages

--- app.js
+++ app.js
@@ -34,25 +34,70 @@ class wizard{
 }
 const bobbyWizard = new wizard('Bobby',50,50)
 
+
+// takes the user from title to grass
 const changeSceneFromTitleToGrass = function(){
     let fromTitle = document.querySelector('#title-screen')
     fromTitle.style.display = 'none';
     let toGrass = document.querySelector('#grass');
     toGrass.style.display = 'flex';
+
+    //show hp display
+    let wizardHp = document.querySelector('#wizard-hp-grass')
+    wizardHp.textContent = `Hp ${bobbyWizard.hp}`
 }
 
 const toGrass = document.getElementById('toGrass')
 
 toGrass.addEventListener('click' , changeSceneFromTitleToGrass)
 
+//Grabs reset by id
+const resetGrass = document.getElementById('reset-grass');
 
-const theRestButton = document.getElementById('reset');
-
-theRestButton.addEventListener('click', function() {
-   const grandparentDiv = theRestButton.parentNode.parentNode;
+resetGrass.addEventListener('click', function() {
+   const grandparentDiv = resetGrass.parentNode.parentNode;
    grandparentDiv.style.display = 'none';
    const toTitle = document.querySelector('#title-screen')
    toTitle.style.display = 'flex';
    bobbyWizard.heal();
 })
 
+
+
+
+
+
+
+
+
+
+//Goes From grass to goblins
+
+const changeSceneFromGrassToGoblin = function(){
+    let fromGrass = document.querySelector('#grass')
+    fromGrass.style.display = 'none';
+    let toGoblin = document.querySelector('#goblin');
+    toGoblin.style.display = 'flex';
+
+    bobbyWizard.getHit(20);
+
+    //show hp display
+    let wizardHp = document.querySelector('#wizard-hp-goblin')
+    wizardHp.textContent = `Hp ${bobbyWizard.hp}`
+}
+
+const toGoblin = document.getElementById('toGoblin')
+
+toGoblin.addEventListener('click' , changeSceneFromGrassToGoblin)
+
+//Grabs reset by id
+const resetGoblin = document.getElementById('reset-goblin');
+
+resetGoblin.addEventListener('click', function() {
+    const grandparentDiv = resetGoblin.parentNode.parentNode;
+    grandparentDiv.style.display = 'none';
+    const toTitle = document.querySelector('#title-screen')
+    toTitle.style.display = 'flex';
+    bobbyWizard.heal();
+ })
+ 
